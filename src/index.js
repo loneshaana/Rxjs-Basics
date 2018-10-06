7c3f8a1d3913b4527e591007a61a6a9700bb8901
shaan <anwarulhaq794@gmail.com>
2018-10-06 11:43:19 +0530

    Iterate Using Observable
    Basic subscribe method Using Rxjs

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { Observable } from "rxjs";
 import "./styles.css";
+
+let numbers = [1, 2, 3, 10, "a"];
+let source = Observable.create(observer => {
+  let index = 0;
+  let produceValue = () => {
+    observer.next(numbers[index++]);
+    if (index < numbers.length) {
+      setTimeout(produceValue, 2000);
+    } else {
+      observer.complete();
+    }
+  };
+  produceValue();
+});
+
+source.subscribe(
+  value => console.log(`value ${value}`),
+  e => console.log(`error ${e}`),
+  () => console.log("Done")
+);
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello RxJs</h1>
     </div>
   );
 }
